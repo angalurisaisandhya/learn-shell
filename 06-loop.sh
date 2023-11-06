507c33302bafe4fce3569c31f32c02e69ab1a240
Sdevops <angalurisaisandhya@gmail.com>
2023-11-06 21:26:36 +0530

    Adding shell topics

---
+++ 06-loop.sh
@@ -0,0 +1,12 @@
+# while and for loop
+
+# choose while if you are using expression
+
+# choose for if you are having inputs
+
+# loop is going to execute same set of commands again and again in iterative way
+a=10
+while [ "$a" -gt 0 ]; do
+  echo Iterative
+  a=$((a-1))
+done
